15d2565717bfaa8d5e53baf106c29443eb5aeea8
haru <estupidyante@gmail.com>
2023-06-07 03:07:40 +0800

    fix layout style

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,53 +1,138 @@
 import { useState } from 'react';
+import ReactSearchBox from 'react-search-box';
 import styled, { keyframes } from 'styled-components';
+import { 
+  MenuOutline,
+  CartOutline,
+  SearchOutline,
+  ArrowForwardOutline
+} from 'react-ionicons';
 
 function App() {
   const [count, setCount] = useState(0);
 
+  const data = [
+    {
+      key: "john",
+      value: "John Doe",
+    },
+    {
+      key: "jane",
+      value: "Jane Doe",
+    },
+    {
+      key: "mary",
+      value: "Mary Phillips",
+    },
+    {
+      key: "robert",
+      value: "Robert",
+    },
+    {
+      key: "karius",
+      value: "Karius",
+    },
+  ];
+
   return (
     <Wrapper>
       <Header>
-        <div>
-          <div data-overlay></div>
-          <LogoLink href="#" target="_self">
-            <Logo src="/assets/images/ezkafe-base-white.png" alt="EzKafe logo" />
-          </LogoLink>
-        </div>
+        <LogoLink href="#" target="_self">
+          <Logo src="/assets/images/ezkafe-base-white.png" alt="EzKafe logo" />
+        </LogoLink>
+        <IconContainer>
+          <CartOutline
+            color={'#ffffff'} 
+            title={''}
+            height="30px"
+            width="30px"
+          />
+          <MenuOutline
+            color={'#ffffff'}
+            title={''}
+            height="30px"
+            width="30px"
+          />
+        </IconContainer>
       </Header>
-      <h1>EzKafe UI</h1>
-      <div>
-        <a href="https://vitejs.dev/" target="_blank">
-          <p>Vite</p>
-        </a>
-        <a href="https://axios-http.com/" target="_blank">
-          <p>Axios</p>
-        </a>
-        <a href="https://reactjs.org/" target="_blank">
-          <p>React</p>
-        </a>
-        <a href="https://styled-components.com/" target="_blank">
-          <p>Styled-Components</p>
-        </a>
-        <a href="https://github.com/aleclarson/vite-tsconfig-paths" target="_blank">
-          <p>Absolute-Path</p>
-        </a>
-        <a href="https://recoiljs.org/" target="_blank">
-          <p>Recoil</p>
-        </a>
-        <a
-          href="https://tanstack.com/query/v4/?from=reactQueryV3&original=https://react-query-v3.tanstack.com/"
-          target="_blank"
-        >
-          <p>React-Query</p>
-        </a>
-      </div>
-      <Card>
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </Card>
-      <ReadTheDocs>Click on the Vite and React logos to learn more</ReadTheDocs>
+      <ContentContainer>
+        <SearchContainer>
+          <SearchBoxContainer>
+            <ReactSearchBox
+              placeholder="Search"
+              data={data} onSelect={function (record: { item: { key: string; value: string; }; }): void {
+                throw new Error('Function not implemented.');
+              } } onChange={function (value: string): void {
+                throw new Error('Function not implemented.');
+              } }      />
+          </SearchBoxContainer>
+          <SearchIconContainer>
+            <SearchOutline
+              color={'#00000'} 
+              title={''}
+              height="30px"
+              width="30px"
+            />
+          </SearchIconContainer>
+        </SearchContainer>
+        <CardHero>
+          <Container>
+            <CardHeroTitle>
+              Best Seller<br/>
+              <strong>of the week</strong>
+            </CardHeroTitle>
+            <CardHeroText>
+              <span>Hot Butterscotch</span>
+              <span>Latte</span>
+            </CardHeroText>
+            <CardHeroButton>
+              <span>More Info</span>
+              <ArrowForwardOutline
+                color={'#ffffff'}
+                title={''}
+                height="18px"
+                width="18px"
+              />
+            </CardHeroButton>
+          </Container>
+        </CardHero>
+        <h1>EzKafe UI</h1>
+        <CardContainer>
+          <Card>
+            <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
+            <p>
+              Edit <code>src/App.tsx</code> and save to test HMR
+            </p>
+          </Card>
+          <Card>
+            <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
+            <p>
+              Edit <code>src/App.tsx</code> and save to test HMR
+            </p>
+          </Card>
+          <Card>
+            <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
+            <p>
+              Edit <code>src/App.tsx</code> and save to test HMR
+            </p>
+          </Card>
+          <Card>
+            <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
+            <p>
+              Edit <code>src/App.tsx</code> and save to test HMR
+            </p>
+          </Card>
+        </CardContainer>
+      </ContentContainer>
+      
+      <Footer>
+        <FooterTop>
+          <FooterBrand>
+
+          </FooterBrand>
+        </FooterTop>
+      </Footer>
+
     </Wrapper>
   );
 }
@@ -62,10 +147,17 @@ const Wrapper = styled.div`
   max-width: 1280px;
   margin: 0 auto;
   text-align: center;
-  padding-top: 115px;
+  padding-top: 100px;
 `;
 
-const Header = styled.div`
+const ContentContainer = styled.div`
+
+`;
+
+const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content:space-between;
   background: #5B3216;
   position: fixed;
   top: 0;
@@ -74,6 +166,11 @@ const Header = styled.div`
   transition: var(--transition-1);
   padding: 1.2em;
   z-index: 4;
+`;
+const IconContainer = styled.div`
+  width: 80px;
+  display: flex;
+  justify-content: space-between;
 `;
 const Logo = styled.img`
   width:160px;
@@ -92,12 +189,101 @@ const LogoLink = styled.a`
   }
 `;
 
-const Card = styled.div`
-  padding: 2em;
+const SearchContainer = styled.div`
+  margin: 1.2em;
+  height: 40px;
+  background: transparent;
+  position: relative;
 `;
 
-const ReadTheDocs = styled.p`
+const SearchBoxContainer = styled.div`
+  width:100%;
+`;
+
+const SearchIconContainer = styled.div`
+  display: flex;
+  -webkit-box-align: center;
+  align-items: center;
+  position: absolute;
+  right: 5px;
+  top: 5px;
+`;
+
+const CardHero = styled.div`
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: left;
+  min-height: 250px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  margin: 2em 1.2em;
+  background: #26140D;
+  border-radius: 10px;
+  box-shadow: 0px 10px 10px 0px rgba(38,20,13,0.30);
+  -webkit-box-shadow: 0px 10px 10px 0px rgba(38,20,13,0.30);
+  -moz-box-shadow: 0px 10px 10px 0px rgba(38,20,13,0.30);
+`;
+
+const Container = styled.div`
+  max-width: 650px;
+  width: 100%;
+  padding-inline: 15px;
+  display: flex;
+  flex-wrap: wrap;
+`;
+const CardHeroTitle = styled.h2`
+  color:#ffffff;
+  margin-bottom: 10px;
+  width:100%;
+  text-align:start;
+`;
+const CardHeroText = styled.p`
+  color: #ffffff;
+  font-family: 'Roboto', sans-serif;
+  font-size: 2rem;
+  line-height: 1.8;
+  max-width: 46ch;
+  margin-bottom: 25px;
+  text-align: start;
+  width:100%;
+`;
+
+const CardHeroButton = styled.div`
+  color: #ffffff;
+  font-size: 1.2rem;
+  display: flex;
+  gap: 10px;
+`;
+
+const CardContainer = styled.div`
+  display:flex;
+  gap: 20px;
+`;
+
+const Card = styled.div`
+  background: rgb(255, 255, 255);
+  border-radius: 20px;
+  height: 300px;
+  width: 200px;
+  border-radius: 10px;
+  box-shadow: 0px 10px 10px 0px rgba(38,20,13,0.30);
+  -webkit-box-shadow: 0px 10px 10px 0px rgba(38,20,13,0.30);
+  -moz-box-shadow: 0px 10px 10px 0px rgba(38,20,13,0.30);
+`;
+
+const Footer = styled.footer`
   color: #888;
+  background: #26140D;
+  width:100%;
+  height:200px;
+`;
+
+const FooterTop = styled.div`
+  color: #888;
+`;
+
+const FooterBrand = styled.div`
 `;
 
 export default App;
--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from 'vite-tsconfig-paths'
+import tsconfigPaths from 'vite-tsconfig-paths';
+import browserslistToEsbuild from 'browserslist-to-esbuild';
 
 export default defineConfig(({ mode }) => {
   /**
@@ -22,6 +23,7 @@ export default defineConfig(({ mode }) => {
   const buildOptions = {
     outDir: "dist",
     assetsDir: "assets",
+    target: browserslistToEsbuild(),
   };
 
   /**
