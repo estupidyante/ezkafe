4b80d36da9ea118314b050b30dd30539e05f7676
haru <estupidyante@gmail.com>
2023-06-13 19:50:19 +0800

    rendering ingredients

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -86,135 +86,136 @@ function App() {
 
   return (
     <Wrapper>
-      <HeaderComponent/>
-      <ContentContainer>
-        <SearchContainer>
-          <SearchBoxContainer>
-            <ReactSearchBox
-              placeholder="Search"
-              data={data} onSelect={function (record: { item: { key: string; value: string; }; }): void {
-                throw new Error('Function not implemented.');
-              } } onChange={function (value: string): void {
-                throw new Error('Function not implemented.');
-              } }      />
-          </SearchBoxContainer>
-          <SearchIconContainer>
-            <SearchOutline
-              color={'#00000'} 
-              title={''}
-              height="30px"
-              width="30px"
-            />
-          </SearchIconContainer>
-        </SearchContainer>
-        <CardHero>
-          <CardHeroContainer>
-            <CardHeroTitle>
-              Best Seller<br/>
-              <strong>of the week</strong>
-            </CardHeroTitle>
-            <CardHeroText>Hot Butterscotch Latte</CardHeroText>
-            <CardHeroButton>
-              <TextSpan>More Info</TextSpan>
+      {!isDetailed && <div>
+        <HeaderComponent/>
+        <ContentContainer>
+          <SearchContainer>
+            <SearchBoxContainer>
+              <ReactSearchBox
+                placeholder="Search"
+                data={data} onSelect={function (record: { item: { key: string; value: string; }; }): void {
+                  throw new Error('Function not implemented.');
+                } } onChange={function (value: string): void {
+                  throw new Error('Function not implemented.');
+                } }      />
+            </SearchBoxContainer>
+            <SearchIconContainer>
+              <SearchOutline
+                color={'#00000'} 
+                title={''}
+                height="30px"
+                width="30px"
+              />
+            </SearchIconContainer>
+          </SearchContainer>
+          <CardHero>
+            <CardHeroContainer>
+              <CardHeroTitle>
+                Best Seller<br/>
+                <strong>of the week</strong>
+              </CardHeroTitle>
+              <CardHeroText>Hot Butterscotch Latte</CardHeroText>
+              <CardHeroButton>
+                <TextSpan>More Info</TextSpan>
+                <ArrowForwardOutline
+                  color={'#ffffff'}
+                  title={''}
+                  height="18px"
+                  width="18px"
+                />
+              </CardHeroButton>
+            </CardHeroContainer>
+          </CardHero>
+          <Section>
+            <h2>EzKafe Drink Menu</h2>
+            {!isOrdered && <Button onClick={handleClickOrder}>
+              <span>See all</span>
               <ArrowForwardOutline
-                color={'#ffffff'}
-                title={''}
-                height="18px"
-                width="18px"
-              />
-            </CardHeroButton>
-          </CardHeroContainer>
-        </CardHero>
-        <Section>
-          <h2>EzKafe Drink Menu</h2>
-          {!isOrdered && <Button onClick={handleClickOrder}>
-            <span>See all</span>
-            <ArrowForwardOutline
-                color={'#26140D'}
-                title={''}
-                height="18px"
-                width="18px"
-              />
-          </Button>}
-        </Section>
-        {!isOrdered && <CardProductContainer>
-          <Container>
-            <CardProductLists>
-            {products.map((item, i) => {
-              if(i >= 2) return ('');
-              return (
-                <CardProductItem key={i}>
-                <CardProduct>
-                  <CardProductImage src={URI + item?.image} alt={item?.name} />
-                  <CardProductContent>
-                    <CardProductTitle>{item?.name}</CardProductTitle>
-                    <CardProductDescription>{item?.description}</CardProductDescription>
-                    <CardProductPrice value="180.85">Php {item?.price}</CardProductPrice>
-                  </CardProductContent>
-                </CardProduct>
-              </CardProductItem>
-              );
-            })}
-    
-            </CardProductLists>
-          </Container>
-        </CardProductContainer>}
-        {!isOrdered && <Section>
-          <Button kind={''} onClick={handleClickOrder}>Order Now</Button>
-        </Section>}
-        {isOrdered && <Section>
-          <DividerLineTop></DividerLineTop>
-          <br/>
-          <br/>
-          <div style={{ backgroundColor: '#ffffff', width: '100%', display: 'flex', borderRadius: 20, borderWidth: 1, borderStyle: 'solid' }}>
-            {categories.map((item, i) => {
-              return(
-                <button key={i} style={{ flex: '1 1 0px', height: '50px', borderRadius: 20, }} onClick={() => {
-                  setActiveSwitch(item?.name);
-                }}>
-                  {item?.name}
-                </button>
-              );
-            })}
-          </div>
-        </Section>}
-      </ContentContainer>
-      {isOrdered && <div style={{ backgroundColor: '#ffffff', width: '100%', height: 'auto', borderStartStartRadius: 20, borderStartEndRadius: 20, borderWidth: 1, borderStyle: 'solid', padding: 20 }}>
-        {products.map((item, i) => {
-            return (
-              <AllProductContainer key={i}>
-                <div style={{ display: 'flex', alignItems: 'center', marginRight: 20, width: 80,}}>
-                  <AllProductImage src={URI + item?.image} alt={item?.name} />
-                </div>
-                <AllProductContentContainer>
-                  <AllProductTitle>{item?.name}</AllProductTitle>
-                  <AllProductDescription>{item?.description}</AllProductDescription>
-                  <AllProductPrice>Php {item?.price}</AllProductPrice>
-                </AllProductContentContainer>
-                <button style={{backgroundColor:'#26140D', margin: 5, display: 'flex', alignItems: 'center'}} onClick={() => {
-                  setIsDetailed(true);
-                  setSelectedProduct(item);
-                }}>
-                  <AddOutline
-                    color={'#ffffff'} 
-                    title={''}
-                    height="30px"
-                    width="30px"
-                  />
-                </button>
-              </AllProductContainer>
-            );
-          })}
-      </div>}
+                  color={'#26140D'}
+                  title={''}
+                  height="18px"
+                  width="18px"
+                />
+            </Button>}
+          </Section>
+          {!isOrdered && <CardProductContainer>
+            <Container>
+              <CardProductLists>
+              {products.map((item, i) => {
+                if(i >= 2) return ('');
+                return (
+                  <CardProductItem key={i}>
+                  <CardProduct>
+                    <CardProductImage src={URI + item?.image} alt={item?.name} />
+                    <CardProductContent>
+                      <CardProductTitle>{item?.name}</CardProductTitle>
+                      <CardProductDescription>{item?.description}</CardProductDescription>
+                      <CardProductPrice value="180.85">Php {item?.price}</CardProductPrice>
+                    </CardProductContent>
+                  </CardProduct>
+                </CardProductItem>
+                );
+              })}
       
-      <Footer>
-        <FooterTop>
-          <FooterBrand>
+              </CardProductLists>
+            </Container>
+          </CardProductContainer>}
+          {!isOrdered && <Section>
+            <Button kind={''} onClick={handleClickOrder}>Order Now</Button>
+          </Section>}
+          {isOrdered && <Section>
+            <DividerLineTop></DividerLineTop>
+            <br/>
+            <br/>
+            <div style={{ backgroundColor: '#ffffff', width: '100%', display: 'flex', borderRadius: 20, borderWidth: 1, borderStyle: 'solid' }}>
+              {categories.map((item, i) => {
+                return(
+                  <button key={i} style={{ flex: '1 1 0px', height: '50px', borderRadius: 20, }} onClick={() => {
+                    setActiveSwitch(item?.name);
+                  }}>
+                    {item?.name}
+                  </button>
+                );
+              })}
+            </div>
+          </Section>}
+        </ContentContainer>
+        {isOrdered && <div style={{ backgroundColor: '#ffffff', width: '100%', height: 'auto', borderStartStartRadius: 20, borderStartEndRadius: 20, borderWidth: 1, borderStyle: 'solid', padding: 20 }}>
+          {products.map((item, i) => {
+              return (
+                <AllProductContainer key={i}>
+                  <div style={{ display: 'flex', alignItems: 'center', marginRight: 20, width: 80,}}>
+                    <AllProductImage src={URI + item?.image} alt={item?.name} />
+                  </div>
+                  <AllProductContentContainer>
+                    <AllProductTitle>{item?.name}</AllProductTitle>
+                    <AllProductDescription>{item?.description}</AllProductDescription>
+                    <AllProductPrice>Php {item?.price}</AllProductPrice>
+                  </AllProductContentContainer>
+                  <button style={{backgroundColor:'#26140D', margin: 5, display: 'flex', alignItems: 'center'}} onClick={() => {
+                    setIsDetailed(true);
+                    setSelectedProduct(item);
+                  }}>
+                    <AddOutline
+                      color={'#ffffff'} 
+                      title={''}
+                      height="30px"
+                      width="30px"
+                    />
+                  </button>
+                </AllProductContainer>
+              );
+            })}
+        </div>}
+        
+        <Footer>
+          <FooterTop>
+            <FooterBrand>
 
-          </FooterBrand>
-        </FooterTop>
-      </Footer>
-
+            </FooterBrand>
+          </FooterTop>
+        </Footer>
+      </div>}
       {isDetailed && <ProductDetailPage product={selectedProduct} state={isDetailed} handleState={handleDetailedState}/>}
     </Wrapper>
   );
--- ui/src/pages/ProductDetailPage.tsx
+++ ui/src/pages/ProductDetailPage.tsx
@@ -10,7 +10,7 @@ import {
 
 export const ProductDetailPage = ({product, state, handleState}) => {
     const [ingredients, setIngredients] = useState([]);
-    let tempIng = [{}];
+    let tempIng = [];
 
     useEffect(() => {
         product?.ing_ids.split(',').map((item, i) => {
@@ -18,19 +18,19 @@ export const ProductDetailPage = ({product, state, handleState}) => {
                 .then((res_ing) => {
                     API.get('types/' + res_ing?.types_id)
                         .then((res_type) => {
-                            tempIng[i] = {
+                            let tempContent = {
                                 id: res_ing?.id,
                                 name: res_ing?.name,
-                                type_id: res_type.id,
-                                type_name: res_type.name
+                                type_id: res_type?.id,
+                                type_name: res_type?.name
                             }
+                            tempIng.push(tempContent);
                         }).finally(() => {
                             setIngredients(tempIng);
-                            console.log(ingredients);
                         })
                 });
         })
-    }, [product]);
+    }, []);
     
     return (
         <ProductDetailedContainer>
@@ -43,20 +43,22 @@ export const ProductDetailPage = ({product, state, handleState}) => {
                     style={{position:'absolute', left: '2rem', top: '2rem'}}
                 />
             </button>
-            <img src={URI + product?.image} alt={product?.name} style={{width:'60%', margin: 'auto'}}/>
-            <div style={{ backgroundColor: '#ffffff', width: '100%', height: '100%', borderStartStartRadius: 20, borderStartEndRadius: 20, borderWidth: 1, borderStyle: 'solid', justifyContent: 'space-evenly', padding: '3rem' }}>
-                <p style={{fontSize:'2rem'}}>{product?.name}</p>
-                <p>Php {product?.price}</p>
-                <button style={{width: '100%', height:50, backgroundColor: '#26140D', color: '#ffffff', borderRadius: 20}} onClick={() => {
+            <img src={URI + product?.image} alt={product?.name} style={{height:250, margin: 'auto', marginBottom:40}}/>
+            <div style={{ backgroundColor: '#ffffff', width: '100%', minHeight: 420, borderStartStartRadius: 20, borderStartEndRadius: 20, borderWidth: 1, borderStyle: 'solid', padding: '3rem' }}>
+                <p style={{fontSize:'2rem',marginBottom:20}}>{product?.name}</p>
+                <p style={{marginBottom:20}}>Php {product?.price}</p>
+                <button style={{width: '100%', height:50, backgroundColor: '#26140D', color: '#ffffff', borderRadius: 10}} onClick={() => {
                     console.log('clicked customized');
                 }}>
                     Customizations
                 </button>
                 <div>
                     {ingredients?.map((ingredients, idx) => {
+                        console.log(ingredients, idx);
                         return (
-                            <div key={idx} style={{borderBottomStyle:'solid',borderBottom:1,borderColor:'#26140D',height:80,display:'flex',alignItems:'center',justifyContent:'space-evenly'}}>
-                                <p style={{display:'flex', justifyContent:'space-evenly',width:'100%'}}><span>{ingredients.type_name}</span> <span>{ingredients.name}</span></p>
+                            <div key={idx} style={{height:80,display:'block',paddingBottom:10}}>
+                                <p style={{display:'flex', justifyContent:'space-evenly', alignItems:'center', width:'100%', height:'100%'}}><span>{ingredients.type_name}</span> <span>{ingredients.name}</span></p>
+                                <hr style={{ backgroundColor:'#26140D'}}/>
                             </div>
                         );
                     })}
@@ -69,9 +71,5 @@ export const ProductDetailPage = ({product, state, handleState}) => {
 const ProductDetailedContainer = styled.div`
     background: #F1DEC9;
     width: 100%;
-    max-height: 1280px;
-    z-index: 100;
-    position: absolute;
-    top: 0;
-    height: 1280px;
+    height: 100%;
 `;
